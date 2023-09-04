e6ed9617cfa679fd3ee6e3cbb7e93b6a8c63a403
Jaeyoon Lee <wodbs9128@gmail.com>
2023-09-04 15:13:04 +0900

    [add] add Counter 2

--- svelte-practice/src/lib/Counter.svelte
+++ svelte-practice/src/lib/Counter.svelte
@@ -1,14 +1,31 @@
 <script>
-  let count = 0;
-  let string = `You have clicked ${count} times`;
+  let count1 = 0;
+  let count2 = 0;
+  $: string1 = `You have clicked ${count1} times`;
+  $: string2 = `You have clicked ${count2} times`;
 
-  function increment() {
-    count++;
+  function setCount2(x) {
+    count2 = x;
+  }
+
+  $: setCount2(count1);
+  $: string = `Count2 is ${count2}`;
+
+  function increment1() {
+    count1++;
   }
 </script>
 
-<button on:click={increment}>Clicks! {count}</button>
+<button on:click={increment1}>Clicks! {count1}</button>
+
 <h3>{string}</h3>
+
+
+<!-- 
+<h3>{string1}</h3>
+<h3>{string2}</h3> -->
+
+<!-- <h3>You have clicked {count} times</h3> -->
 
 <style>
   button {
