6a73473dcec922d1a1f2bcadcea1aa1959d1fbfa
Jaeyoon Lee <wodbs9128@gmail.com>
2023-09-04 14:47:19 +0900

    [add] add Counter

--- svelte-practice/src/App.svelte
+++ svelte-practice/src/App.svelte
@@ -1,15 +1,9 @@
 <script>
-    let name = 'Svelte'
-    let src = 'https://www.rainforest-alliance.org/wp-content/uploads/2021/06/three-toed-sloth-teaser-1-400x400.jpg.webp'
-    let string = 'Some <strong>string</strong> with html.'
+  import Counter from "./lib/Counter.svelte";
 </script>
 
-<img {src} alt="Some text {name}"/>
-<h1>Hello {name.toUpperCase()}!</h1>
-<p>{@html string}</p>
+
+<Counter />
 
 <style>
-  h1 {
-    color: red;
-  }
 </style>
