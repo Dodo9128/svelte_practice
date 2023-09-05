6e964c1c9d8774a1f9dbae26bca208165eab5f7b
Jaeyoon Lee <wodbs9128@gmail.com>
2023-09-05 11:30:47 +0900

    [add] add global scss import_강의랑 다른 해결책 찾아 push

--- svelte-practice/src/lib/Button.svelte
+++ svelte-practice/src/lib/Button.svelte
@@ -4,13 +4,17 @@
 
 <button><slot>FALLBACK</slot></button>
 
-<style>
+<style lang="scss">
     button{
         border: none;
-        background-color: #ff3e00;
+        background-color: $redcolor;
         color: #ffffff;
         padding: 15px 20px;
         font-weight: bold;
         border-radius: 5px;
+        cursor: pointer;
+        &:hover {
+            background-color: $whitecolor;
+        }
     }
 </style>
--- svelte-practice/svelte.config.js
+++ svelte-practice/svelte.config.js
@@ -1,7 +1,27 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// export default {
+// 	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: [vitePreprocess()]
+// };
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+	// for more information about preprocessors
+	preprocess: vitePreprocess()
+};
+
+// export default {
+// 	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess({
+// 		css: {
+// 			preprocessorOptions: {
+// 				scss: {
+// 					prependData: '@import "./src/styles/variables.scss";'
+// 				}
+// 			}
+// 		}
+// 	})
+// };
