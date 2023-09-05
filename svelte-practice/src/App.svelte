7dabffb4e840e6f52900a351797bb2c614e6ab56
Jaeyoon Lee <wodbs9128@gmail.com>
2023-09-05 15:54:58 +0900

    [add] add stype global variables

--- svelte-practice/src/App.svelte
+++ svelte-practice/src/App.svelte
@@ -2,6 +2,7 @@
   import Button from "./lib/Button.svelte"
 </script>
 
+<!-- <Button size="small" shadow bgColor="red" textColor="blue">Button Text</Button> -->
 <Button size="small" shadow>Button Text</Button>
 
 <style>
--- svelte-practice/src/lib/Button.svelte
+++ svelte-practice/src/lib/Button.svelte
@@ -1,19 +1,27 @@
 <script>
     export let size = 'small';
     export let shadow = false;
+    export let bgColor = 'inherit';
+    export let textColor = 'inherit';
+    // export let bgColor = undefined;
+    // export let textColor = undefined;
 </script>
 
 <button
+    style:--buttonBgColor={bgColor}
+    style:--buttonTextColor={textColor}
     class:size-lg={size === 'large'}
     class:size-sm={size === 'small'}
     class:shadow
-        ><slot>FALLBACK</slot></button>
+>
+    <slot>FALLBACK</slot>
+</button>
 
 <style lang="scss">
     button{
         border: none;
-        background-color: #ff3e00;
-        color: #ffffff;
+        background-color: var(--buttonBgColor);
+        color:  var(--buttonTextColor);
         padding: 15px 20px;
         font-weight: bold;
         border-radius: 5px;
@@ -29,10 +37,12 @@
             box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);
         }
         &:hover {
-            background-image: $hovercolor
+            // background-image: $hovercolor
+            background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
         }
         &:active {
-            background-image: $activecolor;
+            // background-image: $activecolor;
+            background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
         }
     }
 </style>
